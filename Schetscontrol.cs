134dcbd23fb0a55af05bc62ebb75f34f7636afe2
MathijsGeurts <45394983+MathijsGeurts@users.noreply.github.com>
2020-11-03 10:13:19 +0100

    Create Schetscontrol.cs

---
+++ Schetscontrol.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace SchetsEditor
+{   public class SchetsControl : UserControl
+    {   private Schets schets;
+        private Color penkleur;
+        public List<TekenObject> getekendeObjecten = new List<TekenObject>();
+
+        public Color PenKleur
+        { get { return penkleur; }
+        }
+        public Schets Schets
+        { get { return schets;   }
+        }
+        public SchetsControl(List<TekenObject> getekendeObjecten = null)
+        {   this.BorderStyle = BorderStyle.Fixed3D;
+            this.schets = new Schets();
+            this.Paint += this.teken;
+            this.Resize += this.veranderAfmeting;
+            this.veranderAfmeting(null, null);
+            if (getekendeObjecten != null)
+            {
+                this.getekendeObjecten = getekendeObjecten;
+
+                foreach (iTekenObject tekenObject in getekendeObjecten)
+                {
+                    tekenObject.Teken(this);
+                }
+            }
+        }
+        public virtual void printGetekendeObjecten()
+        {
+            foreach (TekenObject i in getekendeObjecten)
+            {
+                Console.Write(i.startpunt);
+                Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+        }
+        private void teken(object o, PaintEventArgs pea)
+        {   schets.Teken(pea.Graphics);
+        }
+        private void veranderAfmeting(object o, EventArgs ea)
+        {   schets.VeranderAfmeting(this.ClientSize);
+            this.Invalidate();
+        }
+        public Graphics MaakBitmapGraphics()
+        {   Graphics g = schets.BitmapGraphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            return g;
+        }
+        public void TekenObjecten()
+        {
+            schets.Schoon();
+            foreach (iTekenObject TekenObject in getekendeObjecten)
+            {
+                TekenObject.Teken(this);
+            }
+            this.Invalidate();
+        }
+        public void Schoon(object o, EventArgs ea)
+        {   schets.Schoon();
+            this.Invalidate();
+        }
+        public void Roteer(object o, EventArgs ea)
+        {   schets.VeranderAfmeting(new Size(this.ClientSize.Height, this.ClientSize.Width));
+            schets.Roteer();
+            this.Invalidate();
+        }
+        public void VeranderKleur(object obj, EventArgs ea)
+        {   string kleurNaam = ((ComboBox)obj).Text;
+            penkleur = Color.FromName(kleurNaam);
+        }
+        public void VeranderKleurViaMenu(object obj, EventArgs ea)
+        {   string kleurNaam = ((ToolStripMenuItem)obj).Text;
+            penkleur = Color.FromName(kleurNaam);
+        }
+    }
+}
